afad5575a8fe2d7b9aadc878e08648b8bdc47df1
Travis Bowen <Travisbowen23@gmail.com>
2020-08-04 12:40:48 -0700

    Added some search bar functionality and comments

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+	// Input search bar term
+	state = {
+		searchTerm: null,
+	};
+
+	// Input in search bar function
+	onInputChange(event) {
+		this.setState({
+			searchTerm: event.target.value,
+		});
+	}
+
+	// Prevent form from reloading when hitting enter
+	onFormSubmit = (event) => {
+		event.preventDefault();
+	};
+
 	render() {
 		return (
-			<div>
-				<form>
-					<input type='text' />
+			<div className='ui segment'>
+				<form onSubmit={this.onFormSubmit} className='ui form'>
+					<div className='field'>
+						<label>Image Search</label>
+						<input
+							type='text'
+							value={this.state.searchTerm}
+							onChange={this.onInputChange}
+						/>
+					</div>
 				</form>
 			</div>
 		);
